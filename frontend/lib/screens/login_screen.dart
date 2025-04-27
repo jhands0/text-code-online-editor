7b9b33f7c99693d77b02fc7453bca67c5d539512
Jamie Hands <jamie_hands0@protonmail.com>
2025-04-27 15:13:58 +0100

    feat(ui): added google sign in button with theming and icon

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 20,
+          ),
+          label: const Text(
+            'Sign in with Google',
+            style: TextStyle(
+              color: kBlackColor,
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kWhiteColor,
+            minimumSize: const Size(150, 50),
+          ),
+        ),
+      ),
+    );
   }
 }
