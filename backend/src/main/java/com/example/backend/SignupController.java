a889ed7c65c978fd3e04c8299cacefec0f832945
Jamie Hands <jamie_hands0@protonmail.com>
2025-05-01 16:10:19 +0100

    feat(api): added scaffolding for other methods in signup controller (unimplemented)

--- backend/src/main/java/com/example/backend/SignupController.java
+++ backend/src/main/java/com/example/backend/SignupController.java
@@ -1,6 +1,9 @@
 package com.example.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Arrays;
@@ -10,6 +13,21 @@ public class SignupController {
 
     @GetMapping("/api/signup")
     public List<User> getUser() {
+        return Arrays.asList(new User("name1", "email1", "pfp"), new User("name2", "email2", "pfp"));
+    }
+
+    @PostMapping("/api/signup")
+    public List<user> postUser() {
+        return Arrays.asList(new User("name1", "email1", "pfp"), new User("name2", "email2", "pfp"));
+    }
+
+    @PutMapping("/api/signup")
+    public List<user> putUser() {
+        return Arrays.asList(new User("name1", "email1", "pfp"), new User("name2", "email2", "pfp"));
+    }
+
+    @DeleteMapping("/api/signup")
+    public List<user> deleteUser() {
         return Arrays.asList(new User("name1", "email1", "pfp"), new User("name2", "email2", "pfp"));
     }
 
