2e832ff202a451e8facd426ac1f7c5837d503168
Jamie Hands <jamie_hands0@protonmail.com>
2025-05-02 15:11:27 +0100

    fix(backend): removed id from user model, email will now be the unique value

--- backend/src/main/java/com/example/backend/models/User.java
+++ backend/src/main/java/com/example/backend/models/User.java
@@ -2,20 +2,14 @@ package com.example.backend.models;
 
 
 public class User {
-    private int id;
     private String name;
     private String email;
     private String profilePicture;
 
-    public User(int id, String name, String email, String profilePicture) {
-        this.id = id;
+    public User(String name, String email, String profilePicture) {
         this.name = name;
         this.email = email;
         this.profilePicture = profilePicture;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getName() {
