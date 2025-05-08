b1ddab51ad305a26fbca2fef99e0181496ec547d
Jamie Hands <jamie_hands0@protonmail.com>
2025-05-08 20:09:07 +0100

    feat: added jakarta validation to the document entity

--- backend/src/main/java/com/example/backend/entity/Document.java
+++ backend/src/main/java/com/example/backend/entity/Document.java
@@ -4,15 +4,22 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class Document implements Serializable {
 
+    @NotBlank(message = "Id is mandatory")
     private String id;
 
+    @NotBlank(message = "Title is mandatory")
     private String title;
 
+    @NotBlank(message = "User Id is mandatory")
     private String uid;
 
+    @NotBlank(message = "Content is mandatory")
     private List<String> content;
 
+    @NotBlank(message = "Timestamp is mandatory")
     private LocalDateTime createAt;
 }
