40945ed7ad88ac17e11c224616ddd63c8fe37ea2
Jamie Hands <jamie_hands0@protonmail.com>
2025-05-08 20:11:01 +0100

    feat: added lombok to manage creating instances and getters

--- backend/src/main/java/com/example/backend/entity/Document.java
+++ backend/src/main/java/com/example/backend/entity/Document.java
@@ -5,7 +5,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class Document implements Serializable {
 
     @NotBlank(message = "Id is mandatory")
