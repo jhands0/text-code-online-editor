883fefbd6c694bc14e8061613f8decb588b0f268
Jamie Hands <jamie_hands0@protonmail.com>
2025-05-13 15:45:28 +0100

    feat: added functions to convert and deconvert the document entity to and from json

--- frontend/lib/models/document_model.dart
+++ frontend/lib/models/document_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert'
+
 class DocumentModel {
   final String id;
   final String title;
@@ -11,4 +13,28 @@ class DocumentModel {
     required this.content,
     required this.createdAt,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'uid': uid,
+      'content': content,
+      'createdAt': createdAt,
+    };
+  }
+
+  factory DocumentModel.fromMap(Map<String, dynamic> map) {
+    return DocumentModel(
+      id: map['_id'] ?? '',
+      title: map['title'] ?? '',
+      uid: map['uid'] ?? '',
+      content: List.from(map['content']),
+      createdAt: DateTime.fromMillisecondsSinceEpoch(map['createdAt']), 
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory DocumentModel.fromJson(String source) => DocumentModel.fromMap(json.decode(source));
 }
